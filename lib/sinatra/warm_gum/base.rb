eebedbf6245b2344e4680bdf6076aed067827b6c
pklingem <patrick.klingemann@gmail.com>
2012-04-30 17:10:28 -0400

    stub some responses and implement create and read

--- lib/sinatra/warm_gum/base.rb
+++ lib/sinatra/warm_gum/base.rb
@@ -5,8 +5,49 @@ module Sinatra
   module WarmGum
     module Base
       def self.registered(app)
-        app.get '/:id' do
-          json :id => 1
+        app.get '/user' do
+          json({
+            :user => {
+              :id => 1,
+              :first_name => "Terrible Terry",
+              :last_name => "Tate",
+              :metadata => {}
+            }
+          })
+        end
+
+        app.get '/messages/:id' do
+          json Message.find(params[:id])
+        end
+
+        app.get '/messages' do
+          json([{
+            :message => {
+              :id => 1234,
+              :sent_at => "2012/04/23",
+              :subject => "Get ready for messaging v2",
+              :from => 2,
+              :body => "this is a hot body.",
+              :metadata => {}
+            }
+          }])
+        end
+
+        app.post '/messages' do
+          json Message.create!(params[:message])
+        end
+
+        app.get '/sent' do
+          json([{
+            :message => {
+              :id => 1234,
+              :sent_at => "2012/04/23",
+              :subject => "Get ready for messaging v2",
+              :from => 2,
+              :body => "this is a hot body.",
+              :metadata => {}
+            }
+          }])
         end
       end
     end
