4fe3b1603fcf7b11d458e3a6c40d30889bf7ddbe
pklingem <patrick.klingemann@gmail.com>
2012-05-03 16:58:28 -0400

    implement inbox routes

--- lib/sinatra/warm_gum/inbox.rb
+++ lib/sinatra/warm_gum/inbox.rb
@@ -4,7 +4,20 @@ module Sinatra
   module WarmGum
     module Inbox
       def self.registered(app)
-        #put actions here
+
+        app.get '/inbox' do
+          Message.inbox(@authenticated_user[:id])
+        end
+
+        app.get '/archived' do
+          Message.archived_by_user(@authenticated_user[:id])
+        end
+
+        app.put '/messages/:id/archive' do
+          message = Message.find(params[:id])
+          message.archive!(@authenticated_user[:id])
+        end
+
       end
     end
   end
