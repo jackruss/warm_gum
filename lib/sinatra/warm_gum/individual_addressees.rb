d874b35cba1c96b016e85f7eac8ed594240518fd
pklingem <patrick.klingemann@gmail.com>
2012-05-24 01:18:09 -0400

    relocate individual_addressees routes to proper home in warm_gum

--- lib/sinatra/warm_gum/individual_addressees.rb
+++ lib/sinatra/warm_gum/individual_addressees.rb
@@ -12,23 +12,29 @@ module Sinatra
 
         Message.register_extension_metadata(EXTENSION_METADATA)
 
-        app.put %r{^/messages/(#{ID_FORMAT})/addressees/individual/(\d+)$} do |message_id, individual_addressee_id|
-          if can_read_individual?(individual_addressee_id)
-            @message = Message.find(message_id)
-            @message.add_individual_addressee(individual_addressee_id)
+        app.put %r{^/messages/(#{ID_FORMAT})/addressees/individuals/(\d+)$} do |message_id, individual_id|
+          @message = Message.find(message_id)
+          if @authenticated_user.can_read_individual?(individual_id)
+            @message.add_individual_addressee(individual_id)
             message_json @message
           else
-            halt 403, 'Forbidden'
+            halt 403, json('error' => 'Forbidden')
           end
         end
 
-        app.delete %r{^/messages/(#{ID_FORMAT})/addressees/individual/(\d+)$} do |message_id, individual_addressee_id|
-          if can_read_individual?(individual_addressee_id)
-            @message = Message.find(message_id)
-            @message.remove_individual_addressee(individual_addressee_id)
+        app.get %r{^/messages/(#{ID_FORMAT})/addressees/individuals$} do |message_id|
+          @message = Message.find(message_id)
+          @individuals = @authenticated_user.find_individuals_by_ids(@message.individual_addressees)
+          json(:users => @individuals)
+        end
+
+        app.delete %r{^/messages/(#{ID_FORMAT})/addressees/individuals/(\d+)$} do |message_id, individual_id|
+          @message = Message.find(message_id)
+          if @message.individual_addressee_exists?(individual_id)
+            @message.remove_individual_addressee(individual_id)
             message_json @message
           else
-            halt 403, 'Forbidden'
+            halt 400, json('error' => 'Addressee does not exist')
           end
         end
 
