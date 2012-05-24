13999975a143e42600588a24f21a83df4ba4d6ab
pklingem <patrick.klingemann@gmail.com>
2012-05-24 17:45:47 -0400

    implement GET /messages/:message_id/addressees/groups/:group_type

--- lib/sinatra/warm_gum/group_addressees.rb
+++ lib/sinatra/warm_gum/group_addressees.rb
@@ -24,7 +24,17 @@ module Sinatra
           end
         end
 
+        app.get %r{^/messages/(#{ID_FORMAT})/addressees/groups/(.*)$} do |message_id, group_type|
+          halt 400, json('error' => 'Group type does not exist') unless group_type_exists?(group_type)
+
+          @message = Message.find(message_id)
+          @groups = fetch_groups(@authenticated_user, group_type, @message.group_addressees(group_type))
+          json(:group_type => { :name => group_type, :groups => @groups })
+        end
+
         app.delete %r{^/messages/(#{ID_FORMAT})/addressees/groups/(.*)/(\d+)$} do |message_id, group_type, group_addressee_id|
+          halt 400, json('error' => 'Group type does not exist') unless group_type_exists?(group_type)
+
           if can_read_group?(group_type, group_addressee_id)
             @message = Message.find(message_id)
             @message.remove_group_addressee(group_type, group_addressee_id)
