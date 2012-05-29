bb5aa45acbd655942ae894dc8c91bae3ccf0da8a
pklingem <patrick.klingemann@gmail.com>
2012-05-29 12:12:26 -0400

    implement PUT /messages/:message_id/labels with tests

--- lib/sinatra/warm_gum/labels.rb
+++ lib/sinatra/warm_gum/labels.rb
@@ -3,11 +3,34 @@ require 'sinatra/base'
 module Sinatra
   module WarmGum
     module Labels
+      EXTENSION_METADATA = { 'labels' => [] }
+      METADATA_TRANSFORM = Proc.new do |message, options|
+        { 'labels' => message.labels }
+      end
+
       def self.registered(app)
-        #put actions here
+
+        Message.register_extension_metadata(EXTENSION_METADATA)
+
+        app.put %r{/messages/(#{ID_FORMAT})/labels} do |message_id|
+          @message = Message.find(message_id)
+          if @message.add_label(params[:label])
+            message_json @message
+          else
+            halt 403, json('error' => 'Error adding label')
+          end
+        end
+
+        app.delete %r{/messages/(#{ID_FORMAT})/labels} do |message_id|
+          @message = Message.find(message_id)
+          if @message.remove_label(params[:label])
+            message_json @message
+          else
+            halt 403, json('error' => 'Label does not exist')
+          end
+        end
+
       end
     end
   end
-
-  register WarmGum::Labels
 end
